19ab03dfe9f6e95a1bd5ef09202261a2667c12d4
Vitalii Franchuk <v.franchuk@wnb.com.ua>
2018-01-09 04:26:50 +0200

    Checkout

--- graph/optimizer.py
+++ graph/optimizer.py
@@ -1,10 +1,8 @@
-# Граф
 from copy import deepcopy
 from collections import deque, OrderedDict
 
 graf = []
 tick = 1
-# количество процессоров
 p = 5
 maxLevel = 0
 criticalPath = 0
@@ -12,13 +10,11 @@ CPcopy = criticalPath
 procs = []
 procs1 = []
 matrix = []
-# Данные на шине, передадутся в начале след. такта
 dataOnBus = None
 
 
 class Processor:
     def __init__(self, no, performance):
-        # True если процессор хочет переслать данные и False если задача выполняется
         self.procActive = False
         self.sendEndTime = 0
         # КВВ
@@ -37,15 +33,10 @@ class Processor:
         self.packetSent = False
 
         self.taskEndTime = 0
-        # здесь хранятся пакеты. формат - КОМУ\ОТ\ПРОЧИТАН
+        # receiver\sender\seen
         self.localMemory = []
-        # сколько пакетов пришло
         self.mail = 0
-        # закончил ли выполнение всех заданий
         self.over = 0
-
-        # порядковый номер процессора в топологии, производительность >=1
-        # (кол-во тактов умножается на пр. и округляется вверх)
 
         self.no = no
         from random import randrange
@@ -55,45 +46,25 @@ class Processor:
         print("Processor {} performance {}".format(self.no, self.performance))
         self.taskQueue.clear()
 
-    def __str__(self):
-        return 'Очередь-{0}|{1}|{2}|Выполняется-{3}'.format(len(self.taskQueue), 'Отправляет' if self.isSending else 'Не отправляет', 'Готов к ВВ' if self.hasPacketToSend else 'Не готов к ВВ', self.currentTask)
-
-    # переместить задачу на процессор
     def execute(self, task):
         self.currentTask = task
         self.procActive = True
         self.taskEndTime = tick + task.size * int(self.performance)
 
-    # на каждом такте процессор что-то делает (или простаивает)
     def handle(self):
         # print(self.no)
         global procEnded, dataOnBus
-        if self.over: return
+        if self.over:
+            return
 
-        # if self.isReceiving:
-        #     if self.receiveEndTime == tick:
-        #         закочнили ждать
-                # self.isReceiving = False
-                # self.receiveEndTime = 0
-            # else:
-            #     продолжаем ждать
-                # return
-
-        # self.hasMarker = True if (tick+1) % p == no else False
-
-        #if self.currentTask:
-        # если какая-то задача была выполнена
         if self.taskEndTime == tick:
-            #print('     Задание {0} выполнено'.format(self.currentTask.no))
             for index, j in enumerate(self.currentTask.connections):
                 packet = [j, self.currentTask.no, self.currentTask.connectionWeight[index]]
                 if self.onProc(j):
-                    #print('     Записываем в память')
-                    # размещаем данные в буфере
                     self.localMemory.insert(0, packet)
                     self.mail += 1
                 else:
-                    # или готовим к пересылке
+                    # ready to send
                     self.IOC.append(packet)
                     self.hasPacketToSend = 1
                     if marker.isOccupied is True:
@@ -109,7 +80,6 @@ class Processor:
 
         if self.isSending:
             if self.sendEndTime == tick:
-                # print('     Пересылка задания {0} закончилась'.format(self.IOC[0][1]))
                 self.isSending = False
                 # marker.isOccupied = А
 
@@ -118,8 +88,6 @@ class Processor:
                     self.packetSent = True
                 if not self.IOC:
                     self.hasPacketToSend = 0
-                    # прекращаем передачу, передаем пакет на шину, удаляем задачу из очереди, освобождаем маркер
-                    # print('     Пересылка данных с П-{0}'.format(self.no))
                 self.sendingFrom = -1
                 self.sendingTo = -1
             elif self.sendEndTime == tick + 1:
@@ -134,7 +102,6 @@ class Processor:
                 self.sendEndTime = tick + self.IOC[0][2] * int(self.performance)
                 self.sendingFrom = self.IOC[0][1]
                 self.sendingTo = self.IOC[0][0]
-                # print('     Начинается пересылка с П-{0} - окончание на {1} такте'.format(self.no, self.sendEndTime))
                 self.isSending = True
                 marker.isOccupied = True
                 if self.sendEndTime == tick + 1:
@@ -143,18 +110,13 @@ class Processor:
                     self.packetSent = True
                 else:
                     self.packetSent = False
-                # передача данных
                 return
-                #print('     П-{0} закончил работу и ушел домой к жене'.format(self.no))
             else:
-                # маркера нету, ждем
+                # wait for acess
                 return
 
-        #else:
-        # если очередь не пуста
         if not self.currentTask:
             if self.taskQueue:
-                #костыль
                 while self.mail:
                     for i in self.taskQueue:
                         if i.no == self.localMemory[0][0]:
@@ -191,18 +153,12 @@ class Task:
         self.no = v.no
         self.size = v.wv
         self.connectionWeight = v.wu
-        # Добавляем родителей и их состояние (выполнен/не выполнен)
+        # parents and state (completed or not)
         self.connections = v.u
 
-        # for i in v.u:
-        #    self.parents[i] = False
-    def __str__(self):
-        return '[{0}] Вес - {1}. Родители {2} - {3}'.format(self.no, self.size, self.parentsNo, self.parentsMap)
 
-
-# Вершина графа
 class V:
-    # no - номер, wv - вес вершины, u - массив связей, wu - массив весов связей
+    # no - index, wv - weight, u - edge to, wu - edge weight
     def __init__(self, no, wv, u, wu):
         self.no = no
         self.wv = wv
@@ -214,15 +170,6 @@ class V:
         self.adU = 0
         self.isClusterized = 0
         self.static_level = 0
-        # print(u)
-
-    def __str__(self):
-        return '[{0}] - Вес -{1}. Кластер {2} Родитель для {3} - вес дуг {4}. Статический уровень - {5}'.format(self.no, self.wv, self.isClusterized, self.u, self.wu, self.static_level)
-
-
-def setC(index, array):
-    for i in array:
-        i.isClusterized = index
 
 
 def staticLevel(vertex):
@@ -240,17 +187,17 @@ def staticLevel(vertex):
     return vertex.path
 
 
-class ClusterPool():
+class ClusterManager():
     cell = 1
     clusterizedV = set()
-    #первая ячейка для некластеризованных верщин
+    # first cell is for unclastered
     clusters = [None]
 
     @classmethod
     def printClusters(self):
         for index, i in enumerate(self.clusters):
             if not index:
-                print(' Некластеризованные вершины: ', end='')
+                print(' Unclastered nodes: ', end='')
                 for j in i:
                     print('[{0}]'.format(j.no+1), end=' ')
                 print('. ', end='')
@@ -345,13 +292,13 @@ class ClusterPool():
                     i.adU += 1
                     edge_array.append(index)
 
-            cp1 = findCriticalPath()
+            cp1 = find_critical_path()
 
             if cp1 <= CPcopy:
                 # меняем параметр isClusterized  и соединяем петушков
 
                 self.clusters.remove(cluster2)
-                CPcopy = findCriticalPath()
+                CPcopy = find_critical_path()
 
             else:
                 for index, i in enumerate(cluster1):
@@ -404,7 +351,7 @@ class ClusterPool():
                     i.adU += 1
                     edge_array.append(index)
 
-            newCp = findCriticalPath()
+            newCp = find_critical_path()
             if newCp <= CPcopy:
 
                 self.clusters[0].remove(v2)
@@ -422,7 +369,6 @@ class ClusterPool():
                 del edge_array
 
                 cluster1.remove(v2)
-
 
         elif v2.isClusterized:
             # обнуляем
@@ -456,7 +402,7 @@ class ClusterPool():
                     i.adU += 1
                     edge_array.append(index)
             print(v1, v2)
-            newCp = findCriticalPath()
+            newCp = find_critical_path()
             if newCp <= CPcopy:
 
                 cluster_array = []
@@ -487,7 +433,7 @@ class ClusterPool():
         else:
 
             self.zeroing(v1, [v2])
-            newCp = findCriticalPath()
+            newCp = find_critical_path()
             if newCp <= CPcopy:
                 self.clusters.append([v1, v2])
 
@@ -505,7 +451,7 @@ class ClusterPool():
             for j in i:
                 j.isClusterized = index
 
-        CPcopy = findCriticalPath()
+        CPcopy = find_critical_path()
 
 
 class BusController:
@@ -551,9 +497,9 @@ def build_constructed_graph(g):
             if i.no == 14:
                 pass
             matrix[i.no][j] = i.wu[index1]
-    ClusterPool.clusters[0] = []
+    ClusterManager.clusters[0] = []
     for i in graf:
-        ClusterPool.clusters[0].append(i)
+        ClusterManager.clusters[0].append(i)
 
 
 def setMax(maxL):
@@ -576,30 +522,13 @@ def level(g, vertex):
     vertex.level = maxL + 1
 
 
-def levelDistribution():
-    # определяем самый первый уровень
-    for index, i in enumerate(graf):
-        if not i.parent:
-            i.level = 1
-            # levels[0].append(graf_copy.pop(index))
-
-    for index, j in enumerate(graf):
-        level(graf, j)
-        # for i in graf:
-        # print(i.no, i.level)
-
-
 def successorMaxPath(vertex):
-
     if not vertex.u:
         vertex.path = vertex.wv
         return vertex.wv
     for i in vertex.u:
         if not graf[i].path:
             successorMaxPath(graf[i])
-
-
-
         tmp = graf[i].path + vertex.wu[vertex.u.index(graf[i].no)] + vertex.wv
         if tmp > vertex.path:
             vertex.path = tmp
@@ -607,17 +536,8 @@ def successorMaxPath(vertex):
     return vertex.path
 
 
-'''def criticalPath():
-    for i in graf:
-        if not i.u:
-            i.path = i.wv
-    for i in graf:
-        successorList = i.u
-        for j in successorList:'''
-
-
 # уровни от 1 - оо
-def findCriticalPath():
+def find_critical_path():
     # сначала wipe
     for i in graf:
         i.path = 0
@@ -628,7 +548,6 @@ def findCriticalPath():
             tmp = successorMaxPath(i)
         if tmp > t: t = tmp
     return t
-
 
 
 def findTask(k):
@@ -662,14 +581,11 @@ def setParents():
                         j.parentsMap.append(0)
 
 
-
-def initialLoading():
+def load_tasks():
     global p
-    counter = 0
-    # все вершины на отдельные процессоры
-    for index1, i in enumerate(reversed(ClusterPool.clusters)):
-        if index1 == len(ClusterPool.clusters) - 1:
-            #некластеризованные вершины на разные процессорs
+    for index1, i in enumerate(reversed(ClusterManager.clusters)):
+        if index1 == len(ClusterManager.clusters) - 1:
+            # unclastered to different processors
             for index2, j in enumerate(i):
                 c = Processor(index1 + index2,1)
                 c.taskQueue.append(Task(j))
@@ -684,22 +600,19 @@ def initialLoading():
     setParents()
 
 
-def optimizeGraf():
-    cpool = ClusterPool()
+def optimize_graph():
+    cpool = ClusterManager()
     edges = {}
-    # находим все ребра
+    # all edges
     for i in graf:
         for index, j in enumerate(i.u):
             if i.wu[index] not in list(edges.keys()):
                 edges[i.wu[index]] = []
-            # добавляем ребра в словарь вес - from, to
             edges[i.wu[index]].append([i.no, j])
-    # print(edges)
     global_use = 1
 
     while global_use:
         global_use = 0
-        #print('                     Новый приход')
         for i in reversed(sorted(edges.keys())):
             if i == 0: continue
 
@@ -710,44 +623,16 @@ def optimizeGraf():
                     _from = j[1]
                     _to = j[0]
                     t1 = CPcopy
-                    #print(_to, _from)
                     cpool.clusterize(graf[_to], graf[_from])
-                    # удалось не удалось
+                    # success or fail
                     local_use = 1 if t1 - CPcopy > 0 else 0
 
                     if local_use:
                         edges[i].remove(j)
                         useful +=1
                         global_use +=1
-                        #print('Соединили {0} - {1}. Критический путь - {2}'.format(_to, _from, CPcopy))
                     else:
                         pass
-                        #print('Не получилось соединить {0} - {1}'.format(_to, _from))
-        #print(CPcopy)
-        #ClusterPool.printClusters()
-        # exe
-        #  0, 1, 2, 3  4, 5, 6
-
-def строка_состояния(str):
-    s = '{:<4}p'.format(tick)
-    for  i in str:
-        tmp = ''
-        # if i[0]:
-        #     tmp += 'M|'
-        # else:
-        #     tmp += '-|'
-        if i[1]:
-            tmp += '{:<2}'.format(i[1].no + 1)
-        else:
-            if i[5] > 0:
-                tmp += '{}<-{}'.format(i[5] + 1, i[6])
-            elif i[2]:
-                tmp += '{}->{}'.format(i[3] + 1, i[4] + 1)
-            else:
-                tmp += '-'
-        tmp1 = ' {:<15}p '.format(tmp)
-        s+=tmp1
-    print(s)
 
 
 def convert_status_word(word):
@@ -763,24 +648,6 @@ def convert_status_word(word):
     return res
 
 
-'''matrix = [[0, 2, 0, 0, 0, 0, 0],
-          [0, 0, 10, 0, 0, 0, 0],
-          [0, 0, 0, 3, 0, 0, 0],
-          [0, 0, 0, 0, 2, 0, 0],
-          [0, 0, 0, 0, 0, 4, 0],
-          [0, 0, 0, 0, 0, 0, 5],
-          [0, 0, 0, 0, 0, 0, 0]]'''
-#   0   1   2   3   4   5   6   7   8   9   10  11  12  13  14   15
-'''matrix  = [[0,  0,  1,  0,  5,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,   0],
-           [0,  0,  0,  0,  1,  0,  20,]]
-matrix =    [[0,   1000,  5,   0,   0],
-             [0,   0,  0, 1000,   0],
-             [0,   0,  0,   0, 1000],
-             [0,   0,  0,   0,   0],
-             [0,   0,  0,   0,   0]]
-N = len(matrix)
-vertexWeight = [100, 100, 100, 100, 100]'''
-
 marker = BusController()
 
 
@@ -789,26 +656,14 @@ def perform_simulation(g=None):
 
     build_constructed_graph(g)
 
-    #levelDistribution()
-    CPcopy = findCriticalPath()
+    CPcopy = find_critical_path()
     print(CPcopy)
-    # print(successorMaxPath(graf[0]))
-    criticalPath = findCriticalPath()
-    # print(criticalPath)
-    # print('max = ', maxLevel)
-    condition = True
 
-    graf1 = deepcopy(graf)
+    optimize_graph()
 
-    '''for i in ClusterPool.clusters:
-       if i: print(i)'''
+    load_tasks()
 
-    #print(findCriticalPath())
-    optimizeGraf()
-
-    initialLoading()
-
-    ClusterPool.printClusters()
+    ClusterManager.printClusters()
     procEnded = len(procs)
     status_word = [0 for i in procs]
     table = []
@@ -839,11 +694,11 @@ def perform_simulation(g=None):
         table.append([convert_status_word(w) for w in status_word])
         tick += 1
 
-    # print(findCriticalPath())
+    # print(find_critical_path())
 
     return {
         "table": table,
-        "clusters": ClusterPool.getClusters()
+        "clusters": ClusterManager.getClusters()
     }
 
 
